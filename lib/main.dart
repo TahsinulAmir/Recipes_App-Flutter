5203ff6768f5c379012e81b5ee84954838108b50
TahsinulAmir <tahsinulamir1@gmail.com>
2023-01-21 00:21:42 +0700

    feat: Create category screen & memisahkan body

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipes_app/category_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,12 +23,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Recipes"),
-      ),
-      body: Center(
-        child: Text("Recipes"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Recipes"),
+        ),
+        body: CategoryScreen());
   }
 }
