4c740559c81d09a958f5b3601b3ba289dc527616
TahsinulAmir <tahsinulamir1@gmail.com>
2023-01-22 13:50:04 +0700

    Styling & menampilkan data di detail_food.dart

--- lib/detail_food.dart
+++ lib/detail_food.dart
@@ -7,9 +7,39 @@ class DetailFood extends StatelessWidget {
   Widget build(BuildContext context) {
     Map<String, dynamic> argsFood =
         ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    List<String> ingredients = argsFood['ingredients'];
     return Scaffold(
-      body: Center(
-        child: Text(argsFood['title']),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.only(top: 100, right: 20, left: 20),
+        child: Column(
+          children: [
+            Text(argsFood['title'],
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Ingredients',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                Text('${ingredients.length} items'),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: ingredients.length,
+                itemBuilder: (context, index) {
+                  final ing = ingredients[index];
+                  return Text(ing);
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
