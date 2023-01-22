0eb8c568c80b11c18ddbe5fa93fc6ad15f64fe6e
TahsinulAmir <tahsinulamir1@gmail.com>
2023-01-22 09:42:23 +0700

    Meng oper dan menampilkan data dummyCategories ke Category_screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
-  const CategoryItem({super.key});
+  final String id;
+  final String title;
+  final String images;
+
+  const CategoryItem(
+      {super.key, required this.id, required this.title, required this.images});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.amber,
+      child: Text(title),
     );
   }
 }
--- lib/category_screen.dart
+++ lib/category_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recipes_app/category_item.dart';
+import 'package:recipes_app/dummy_data.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
@@ -9,12 +10,19 @@ class CategoryScreen extends StatelessWidget {
     return GridView.builder(
       padding: EdgeInsets.all(15),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisSpacing: 15,
-          crossAxisSpacing: 15,
-          childAspectRatio: 10 / 12),
+        crossAxisCount: 2,
+        mainAxisSpacing: 15,
+        crossAxisSpacing: 15,
+        childAspectRatio: 10 / 12,
+      ),
+      itemCount: dummyCategories.length,
       itemBuilder: (context, index) {
-        return CategoryItem();
+        final category = dummyCategories[index];
+        return CategoryItem(
+          id: category.id,
+          title: category.title,
+          images: category.images,
+        );
       },
     );
   }
